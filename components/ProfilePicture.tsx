e5a8e9e74461492c2270b4842b2250c527e97223
IRadics <radics_istvan@hotmail.com>
2023-02-05 17:34:05 +0100

    Added className prop to profilePic

--- components/ProfilePicture.tsx
+++ components/ProfilePicture.tsx
@@ -6,13 +6,16 @@ import defaultImage from "../assets/images/profilePicDefault.png";
  * Profile picture component
  * @param src - optional - source to the image. if not provided, the default placeholder will be used
  * @param size - small or large
+ * @param className
  */
 const ProfilePicture = ({
   src,
   size,
+  className,
 }: {
   src?: string;
   size: "small" | "large";
+  className?: string;
 }) => {
   if (!src) src = defaultImage.src;
   const [imageSrc, setImageSrc] = useState<string>(src);
@@ -25,7 +28,7 @@ const ProfilePicture = ({
 
   return (
     <div
-      className={`rounded-full border border-uicolor-70 w-[${sizePx}px] h-[${sizePx}px] overflow-hidden`}
+      className={`rounded-full border border-uicolor-70 w-[${sizePx}px] h-[${sizePx}px] overflow-hidden ${className}`}
     >
       <Image
         className={`rounded-full w-[${sizePx}px] h-[${sizePx}px] object-cover`}
