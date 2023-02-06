4fd2773d034b6a4e911e22f57db6c6820bd8771d
IRadics <radics_istvan@hotmail.com>
2023-02-07 00:46:46 +0100

    Added error prop to TextInput

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,6 +1,8 @@
 import React, { InputHTMLAttributes } from "react";
 
-export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+}
 
 /**
  * Text input element
@@ -8,14 +10,20 @@ export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
  */
 const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
   (props, ref) => {
-    const { className, ...htmlInputProps } = props;
+    const { className, error, ...htmlInputProps } = props;
     return (
       <input
         className={`w-full h-10 px-3 py-2  rounded-lg  border 
      placeholder:text-textcolor-disabled
      bg-uicolor-80  border-uicolor-60
      hover:bg-uicolor-80 hover:border-uicolor-30
-     focus:bg-uicolor-60 focus:border-uicolor-10 ${className}`}
+     focus:bg-uicolor-60 focus:border-uicolor-10 
+     ${
+       error
+         ? "border-uicolor-error hover:border-uicolor-error focus:border-uicolor-error"
+         : ""
+     }
+     ${className}`}
         ref={ref}
         {...htmlInputProps}
       ></input>
