e02b579f7b6bd679c1f8f0942aae925374067a73
IRadics <radics_istvan@hotmail.com>
2023-03-10 23:56:43 +0100

    Another attempt to fix db file in prod build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-const path = require("path");
+const paths = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 const nextConfig = {
@@ -14,19 +13,25 @@ const nextConfig = {
 
     //copy over DB file for production build if it has been provided as env. variable
     const path = process.env.DATABASE_URL.replace(/file:\.?(.*)/, "$1");
-    if (!path) return;
+    if (!path) return config;
     const absolutePath = process.env.DATABASE_URL.startsWith("file:/");
 
     //absolute path won't work with the current method, while as Prisma picks up at the absolute folder from system root, webpack does it in build folder
-    if (absolutePath) return;
+    if (absolutePath) return config;
 
-    if (isServer && !dev) {
+    //if client, the output path is the root .next folder - this is what we need.
+    if (!isServer && !dev) {
+      const root = require("path").resolve(__dirname, "./");
+      const outputPath = config.output.path;
+      const from = `${root}/prisma/${path}`;
+      const to = `${outputPath}/../prisma/[name][ext]`;
+
       config.plugins.push(
         new CopyPlugin({
           patterns: [
             {
-              from: `./prisma/${path}`,
-              to: "../../prisma/[name][ext]",
+              from: from,
+              to: to,
             },
           ],
         })
