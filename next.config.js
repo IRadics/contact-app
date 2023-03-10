0386123ef279439a2ba3612ec4733ec84d751afa
IRadics <radics_istvan@hotmail.com>
2023-03-10 23:13:53 +0100

    the DB file is now correctly copied over to prod build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,40 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    if (!process.env.DATABASE_URL) return;
+
+    //copy over DB file for production build if it has been provided as env. variable
+    const path = process.env.DATABASE_URL.replace(/file:\.?(.*)/, "$1");
+    if (!path) return;
+    const absolutePath = process.env.DATABASE_URL.startsWith("file:/");
+
+    //absolute path won't work with the current method, while as Prisma picks up at the absolute folder from system root, webpack does it in build folder
+    if (absolutePath) return;
+
+    if (isServer && !dev) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: `./prisma/${path}`,
+              to: "../../prisma/[name][ext]",
+            },
+          ],
+        })
+      );
+    }
+    // Important: return the modified config
+    return config;
+  },
+};
+
+module.exports = nextConfig;
