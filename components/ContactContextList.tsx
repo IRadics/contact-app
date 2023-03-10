91b1f41254dec16406d8db0eb4bdd56cc6616606
IRadics <radics_istvan@hotmail.com>
2023-03-10 15:35:48 +0100

    Possible fix for prod build .map not function err

--- components/ContactContextList.tsx
+++ components/ContactContextList.tsx
@@ -12,26 +12,28 @@ const ContactContextList = ({
 
   return (
     <div className={`w-56 rounded-lg overflow-hidden ${className} z-10`}>
-      {items.map((item) => (
-        <button
-          className="flex flex-row h-11 w-full bg-uicolor-80 hover:bg-uicolor-70 active:bg-uicolor-60 p-3 gap-3 max-md:flex-row-reverse"
-          key={item.icon.src + item.label}
-          onMouseDown={() => {
-            item.action(item.contact);
-          }}
-        >
-          <Image
-            width={sizePx}
-            height={sizePx}
-            src={item.icon}
-            alt=""
-            //dirty solution for svg "coloring" - TODO: replace with a better solution (actually coloring the svg)
-            className="text-current opacity-[.56]"
-          />
+      {items &&
+        items.length > 0 &&
+        items.map((item) => (
+          <button
+            className="flex flex-row h-11 w-full bg-uicolor-80 hover:bg-uicolor-70 active:bg-uicolor-60 p-3 gap-3 max-md:flex-row-reverse"
+            key={item.icon.src + item.label}
+            onMouseDown={() => {
+              item.action(item.contact);
+            }}
+          >
+            <Image
+              width={sizePx}
+              height={sizePx}
+              src={item.icon}
+              alt=""
+              //dirty solution for svg "coloring" - TODO: replace with a better solution (actually coloring the svg)
+              className="text-current opacity-[.56]"
+            />
 
-          {item.label}
-        </button>
-      ))}
+            {item.label}
+          </button>
+        ))}
     </div>
   );
 };
